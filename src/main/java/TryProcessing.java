3b41b3735a80c99edc2f8cee71265aaee6245643
jyotijauhari <jyotijauhari222@gmail.com>
2022-07-15 13:26:01 +0530

    :memo: processing setup

---
+++ src/main/java/TryProcessing.java
@@ -0,0 +1,13 @@
+import processing.core.PApplet;
+public class TryProcessing extends PApplet{
+    public static void main(String[] args) {
+        PApplet.main("TryProcessing",args );
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(648, 488);
+    }
+
+}
