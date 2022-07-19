0c5ff01a1c5c7a0cdbbb12a1c60b250e6e45ff15
jyotijauhari <jyotijauhari222@gmail.com>
2022-07-19 16:59:28 +0530

    settings, setup, draw methods

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -1,5 +1,8 @@
 import processing.core.PApplet;
 public class TryProcessing extends PApplet{
+    public static final int WIDTH = 648;
+    public static final int HEIGHT = 488;
+
     public static void main(String[] args) {
         PApplet.main("TryProcessing",args );
     }
@@ -7,7 +10,17 @@ public class TryProcessing extends PApplet{
     @Override
     public void settings() {
         super.settings();
-        size(648, 488);
+        size(WIDTH , HEIGHT);
     }
 
+    @Override
+    public void setup() {
+        System.out.println("print from setup method");
+        ellipse(WIDTH/2,HEIGHT/2,100,100);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("from draw method"); // keep printing contimously till we dont close processing gui window
+    }
 }
